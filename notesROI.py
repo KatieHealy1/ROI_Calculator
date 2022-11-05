c413cc5bd85f9ddb26372d490eab8b75270ce5c2
KatieHealy1 <khealy001@gmail.com>
2022-11-05 11:13:11 -0500

    first commit

---
+++ notesROI.py
@@ -0,0 +1,61 @@
+# four sq method:
+    # top left box:
+        #income: (from property) for ex the duplex costs $200,000 and since it is a duplex there are 2 units for rent which are $1000 each
+            # rental income = $2,000/month
+            # laundry machine income = $0
+            # storage unit = $0
+            # misc = $0
+            # total monthly income = $2,000/month
+    
+    # bottom left box:
+        # expenses:
+            # tax = $150/month
+            # insurance = $100/month
+            # utilities = $0 paid by renter
+                # electric = $0 paid by renter
+                # water = $0 paid by renter
+                # sewer = $0 paid by renter
+                # garbage = $0 paid by renter
+                # gas = $0 paid by renter
+            # HOA = $0 
+            # lawn/snow care = $0 
+            # vacancy = 5% of rental income $100/month
+            # repairs = $100/month between the 2 units
+            # capital expenditures = $100/month between the 2 units
+            # property management = $200/month
+            # mortgage = $860/month
+            # total monthly expenses = $1,610
+
+    # top right box:
+        # cashflow:
+            # income - expenses
+            # 2,000 - 1,610 = $390
+            # total monthly cash flow = $390
+
+    # bottom right box:
+        # cash on cash ROI:
+            # down payment = $40,000
+            # closing cost = $3,000
+            # rehab budget = $7,000
+            # misc = $0
+            # total investment = $50,000
+        # take monthly cashflow X 12 = $4,680 annually
+        # to figure oout cash on cash return you are 
+        # going to divide annual cash flow by total investment
+        # $4,680 / $50,000 = 9.36%
+        # cash on cash ROI = 9.36%
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
